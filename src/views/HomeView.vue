36f7f3733fef2b97b7757a62d89e3d36326d2f6f
IrinaX <iksanova08@mail.ru>
2025-04-11 01:50:20 +0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import VPromo from '../components/VPromo.vue'
+import VClients from '../components/VClients.vue'
+import VManage from '../components/VManage.vue'
+import VAchievements from '../components/VAchievements.vue'
+import VCustomer from '../components/VCustomer.vue'
+import VCta from '../components/VCta.vue'
+</script>
+
+<template>
+  <main>
+    <VPromo />
+    <VClients />
+    <VManage />
+    <VAchievements />
+    <VCustomer />
+    <VCta />
+  </main>
+</template>
